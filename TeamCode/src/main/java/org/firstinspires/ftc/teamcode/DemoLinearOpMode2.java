a89a479c78577839cf88812ac7c0561bd62a2221
saanson84 <saanson84@gmail.com>
2020-05-28 12:41:01 -0500

    Initial commit of 4 Demo Opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoLinearOpMode2.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name = "Demo Linear OpMode")
+public class DemoLinearOpMode2 extends LinearOpMode {
+    private DcMotor leftMotor;
+    private Servo servo;
+    private OpticalDistanceSensor ods;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftMotor = hardwareMap.dcMotor.get("left");
+        servo = hardwareMap.servo.get("servo");
+        ods = hardwareMap.opticalDistanceSensor.get("ods");
+
+        waitForStart();
+
+        leftMotor.setPower(1);
+        servo.setPosition(1);
+        double light = ods.getLightDetected();
+
+        Thread.sleep(1000);
+        if (light >= 0.5) {
+            servo.setPosition(light);
+        } else {
+            servo.setPosition(0);
+        }
+        leftMotor.setPower(0);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoOpMode.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "demo teleop")
+public class DemoOpMode extends OpMode {
+    private DcMotor leftMotor;
+    private Servo servo;
+
+
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.dcMotor.get("left");
+        servo = hardwareMap.servo.get("servo");
+
+    }
+
+    @Override
+    public void loop() {
+        leftMotor.setPower(gamepad1.left_stick_y);
+        if (gamepad1.a) {
+            servo.setPosition(1);
+        } else {
+            servo.setPosition(0);
+        }
+    }
+}
